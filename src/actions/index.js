afc8b515d95b3f0e48ca9b6adec52de65e8d0d56
nshaiva14 <nikita.shaiva.18@dartmouth.edu>
2017-05-10 10:09:58 -0400

    ok

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,9 @@
 import axios from 'axios';
 
-const ROOT_URL = 'https://cs52-blog.herokuapp.com/api';
+// const ROOT_URL = 'http://localhost:9090/api';
+const ROOT_URL = 'https://lab5-nshaiva14.herokuapp.com/api';
+// const ROOT_URL = 'https://cs52-blog.herokuapp.com/api';
+
 const API_KEY = '?key=Nikita_Shaiva2';
 
 // keys for actiontypes
